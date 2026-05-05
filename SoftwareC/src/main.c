83c243651eda9470b25c130a5bd55e48d85c5a19
Fernando Daniele <fernandodaniele1993@gmail.com>
2026-05-05 16:53:25 +0200

    ProjectStart-Dev1

--- SoftwareC/src/main.c
+++ SoftwareC/src/main.c
@@ -12,8 +12,35 @@
 #include <stdlib.h>
 #include "../inc/menu.h"
 
-int main()
-{
+/**
+ * @brief Para test
+ * 
+ * @return int 
+ */
+void menuInit(){
+    printf( "Inicio");
+}
+void menuUpdate(char a){
+    printf( "Letra: %c ", a);
+}
+
+int main(){
+    char letter = ' ';
+    //Seccion de configuracion/inicializacion
+    menuInit();
+
+    //Loop infinito
+    while(1)
+    {
+        //Leer entrada de teclado
+
+        //si se presiona Escape
+        //salir del programa (break)
+        
+        //si se presiona otra tecla
+        //pasar el carácter correspondiente como parámetro a la función void menuUpdate(char);
+        menuUpdate(letter);
+    }
 
     return 0;
 }
